6ae0182f0e718b91429e5873b3979363db1a1eaa
AndrosDe <andreas_beier1981@yahoo.de>
2022-05-12 13:31:13 +0000

    add game tests

---
+++ scripts/game.js
@@ -0,0 +1,20 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1","button2","button3","button4"],
+}
+
+// function newGame()
+
+// function addTrun()
+
+// function showTurn()
+
+// function lightsOn()
+
+// function playerTurn()
+
+// function showScore()
+
+module.exports = { game };
